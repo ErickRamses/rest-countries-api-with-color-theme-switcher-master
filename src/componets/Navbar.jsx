969f1f9c11a36e39d23ac7a9fbb20f1ce89efd32
ErickRamses <105986183+ErickRamses@users.noreply.github.com>
2022-07-09 10:02:58 -0700

    2do

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { useRef } from 'react'
+import { useEffect } from 'react'
+
 
 export const Navbar = () => {
+  
+  const input =useRef()
+
+  
+  //useEffect(()=>{console.log("d")},input.current.value)
+
+  //make it in app
   return (
-    <div>Navbar</div>
+    <div>
+      <input ref={input} onChange={()=>{console.log(input.current.value)}} type="text"/>
+    </div>
   )
 }
